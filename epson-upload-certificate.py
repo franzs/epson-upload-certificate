3306fef4ba64fa5fd8a187c3f08738e72455c1b3
Franz Schwartau <425188+franzs@users.noreply.github.com>
2026-04-19 23:30:36 +0200

    Reformat using black

--- epson-upload-certificate.py
+++ epson-upload-certificate.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3
+
 import requests, html5lib, io
 
 URL = 'https://myepson.example.com/'
@@ -11,14 +12,17 @@ CERTFILE = '/home/majid/web/acme-tiny/epson.crt'
 # step 1, authenticate
 jar = requests.cookies.RequestsCookieJar()
 set_url = URL + 'PRESENTATION/ADVANCED/PASSWORD/SET'
-r = requests.post(set_url, cookies=jar,
-                  data={
-                    'INPUTT_USERNAME': USERNAME,
-                    'access': 'https',
-                    'INPUTT_PASSWORD': PASSWORD,
-                    'INPUTT_ACCSESSMETHOD': 0,
-                    'INPUTT_DUMMY': ''
-                  })
+r = requests.post(
+    set_url,
+    cookies=jar,
+    data={
+        'INPUTT_USERNAME': USERNAME,
+        'access': 'https',
+        'INPUTT_PASSWORD': PASSWORD,
+        'INPUTT_ACCSESSMETHOD': 0,
+        'INPUTT_DUMMY': '',
+    },
+)
 assert r.status_code == 200
 jar = r.cookies
 
@@ -27,8 +31,7 @@ jar = r.cookies
 form_url = URL + 'PRESENTATION/ADVANCED/NWS_CERT_SSLTLS/CA_IMPORT'
 r = requests.get(form_url, cookies=jar)
 tree = html5lib.parse(r.text, namespaceHTMLElements=False)
-data = dict([(f.attrib['name'], f.attrib['value']) for f in
-             tree.findall('.//input')])
+data = dict([(f.attrib['name'], f.attrib['value']) for f in tree.findall('.//input')])
 assert 'INPUTT_SETUPTOKEN' in data
 
 # step 3, upload key and certs
@@ -48,27 +51,30 @@ full = f.readlines()
 f.close()
 certno = 0
 certs = dict()
+
 for line in full:
-  if not line.strip(): continue
-  certs[certno] = certs.get(certno, '') + line
-  if 'END CERTIFICATE' in line:
-    certno = certno + 1
+    if not line.strip():
+        continue
+    certs[certno] = certs.get(certno, '') + line
+    if 'END CERTIFICATE' in line:
+        certno = certno + 1
+
 files = {
-  'key': open(KEYFILE, 'rb'),
+    'key': open(KEYFILE, 'rb'),
 }
+
 for certno in certs:
-  assert certno < 3
-  files[f'cert{certno}'] = io.BytesIO(certs[certno].encode('utf-8'))
+    assert certno < 3
+    files[f'cert{certno}'] = io.BytesIO(certs[certno].encode('utf-8'))
 
 ########################################################################
 # step 3, submit the new cert
-r = requests.post(upload_url, cookies=jar,
-                  files=files,
-                  data=data)
+r = requests.post(upload_url, cookies=jar, files=files, data=data)
 
 ########################################################################
 # step 4, verify the printer accepted the cert and is shutting down
 if not 'Shutting down' in r.text:
-  print(r.text)
+    print(r.text)
 assert 'Shutting down' in r.text
+
 print('Epson certificate successfully uploaded to printer.')
